a0a4b85f2da94749c9d3a7632903529d768162d7
JYPark <jypark.t1@gmail.com>
2025-02-02 18:02:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/user/:id', (req, res) => {
 
 app.get('/sound/:name', (req, res) => {
     const { name } = req.params;
+    console.log(name);
     if (name === 'dog') {
         res.send({'sound': '멍멍'});
     } else if (name === 'cat') {
@@ -34,7 +35,7 @@ app.get('/sound/:name', (req, res) => {
     } else {
         res.send({'sound': '알수없음'});
     }
-    console.log(name);
+    
 })
 
 
